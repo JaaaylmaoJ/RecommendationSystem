b874e01846e5e600e5ab5da7af85a7760284732e
JoyJ <nikichns@rambler.ru>
2020-05-20 23:33:04 +0500

    в начале был файл индекса...

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo    '<div class="container" style="height: auto; display: block; margin: 0 auto; padding-top: 40vh; width: 900px;">
+            <h1 style="text-align: center;">Здравствуйте , здесь что-то когда-то обновится</h1>
+        </div>';
+
+?>
+
+
+
